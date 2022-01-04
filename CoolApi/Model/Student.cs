03135a2a2d76882650f778547b9372b42b31867a
Krzysztof <Krzysztofbojarczuk@wp.pl>
2022-01-04 15:36:35 +0100

    Add project files.

---
+++ CoolApi/Model/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace CoolApi.Model
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
---
+++ CoolApi/Model/StudentContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoolApi.Model
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
